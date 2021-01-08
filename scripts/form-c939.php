a4205081ea5cdaac1bc02a502d0ed0859d2c418d
Mayville <MAYVILLEFD18@GCC.EDU>
2021-01-08 14:17:17 -0500

    Page Update

---
+++ scripts/form-c939.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'email' => array(
+    'from' => 'admin@arms-search.com',
+    'to' => 'admin@arms-search.com'
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
